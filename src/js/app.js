d7d9637cc4405cc8c87796ba422903af1ba50ae2
blogofadam <79380393+blogofadam@users.noreply.github.com>
2021-02-21 12:04:00 -0800

    Update app.js
    Line linked false

--- src/js/app.js
+++ src/js/app.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       },
       "line_linked": {
-        "enable": true,
+        "enable": false,
         "distance": 150,
         "color": "#ffffff",
         "opacity": 0.4,
